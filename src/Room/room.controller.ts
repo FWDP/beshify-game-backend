20c4918febed13ed8a4e0aec04e0ada1e3dae3f5
xyon3 <reyespauldavid@gmail.com>
2023-07-22 21:08:55 +0800

    feat: add dynamic chat connection for rooms

--- src/Room/room.controller.ts
+++ src/Room/room.controller.ts
@@ -2,8 +2,25 @@ import { Router, Request, Response } from "express";
 import RoomService from "./room.service";
 import { checkSchema, validationResult } from "express-validator";
 import { createRoomSchema, joinRoomSchema } from "./room.schema";
+import { io } from "../server";
 
 const RoomController: Router = Router();
+
+io.on("connection", async (socket) => {
+  RoomService.chatRooms.map((rID: String) => {
+    socket.on(`${rID}:connect`, (data) => {
+      socket.broadcast.emit(`${rID}:chat`, `${data} joined the game`);
+    });
+
+    socket.on(`${rID}:chat`, (data) => {
+      socket.broadcast.emit(`${rID}:chat`, data);
+    });
+
+    socket.on(`${rID}:disconnect`, (data) => {
+      socket.broadcast.emit(`${rID}:chat`, `${data} left the game`);
+    });
+  });
+});
 
 RoomController.get("/", async (req: Request, res: Response) => {
   res.json(await RoomService.getAllRooms());
@@ -48,4 +65,29 @@ RoomController.patch("/", async (req: Request, res: Response) => {
     return res.status(500).send(`${err}`);
   }
 });
+
+/* Codes below are for testing purposes */
+RoomController.delete("/", async (req: Request, res: Response) => {
+  try {
+    res.json(await RoomService.deleteRoom(req.body.id));
+  } catch (err) {
+    return res.status(500).send(`${err}`);
+  }
+});
+
+RoomController.get("/chat", async (req: Request, res: Response) => {
+  try {
+    res.json(await RoomService.getChatRooms());
+  } catch (err) {
+    return res.status(500).send(`${err}`);
+  }
+});
+RoomController.get("/chat/re", async (req: Request, res: Response) => {
+  try {
+    res.json(await RoomService.repopulateChatRooms());
+  } catch (err) {
+    return res.status(500).send(`${err}`);
+  }
+});
+
 export default RoomController;
