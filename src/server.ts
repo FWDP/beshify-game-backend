7aab4870cb82faf8838089e2bd59ae4782daf3f8
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2023-07-24 20:16:34 +0800

    Feat: using Netlify, does not work; using ngrok

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-
 const app: Express = express();
 const httpServer = createServer(app);
 
@@ -22,20 +21,7 @@ app.get("/", (req: Request, res: Response) => {
 
 import { AppModule } from "./serverModule";
 AppModule.Load(app);
-if (process.env.NODE_ENV !== "production") {
-  console.log("dev mode");
-  httpServer.listen(port, () => {
-    console.log(`serving on ${port}`);
-  });
-}
 
-// Export the Express app as a function for Vercel
-export default (req: Request, res: Response) => {
-  // Allow only GET requests for the root path ("/")
-  if (req.method === "GET" && req.url === "/") {
-    return app(req, res);
-  }
-
-  // For all other requests, return 404
-  res.status(404).send("Not found");
-};
+httpServer.listen(port, () => {
+  console.log(`serving on ${port}`);
+});
