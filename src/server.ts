ea55bbaf29a80f938db1aeaaa6d080254e00af78
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2023-08-03 13:42:27 +0800

    feat: Add CORS

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 const app: Express = express();
 const httpServer = createServer(app);
 
-// Temporary * for orign
+// Temporary * for origin
 export const io = new Server(httpServer, {
   cors: {
     origin: "*",
@@ -14,6 +15,7 @@ export const io = new Server(httpServer, {
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Beshify API");
